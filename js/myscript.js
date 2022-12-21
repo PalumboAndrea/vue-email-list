a63d7d5e29f129b5ed0b5e307ca38f6dd0381c9a
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-21 16:04:02 +0100

    fix the list

--- js/myscript.js
+++ js/myscript.js
@@ -11,13 +11,9 @@ createApp({
             for(i=0; i<10; i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then( (response) => {
-                console.log(response.data.response)
                 this.emailList.push(response.data.response);
-                
             })
-            
             }
-            console.log(this.emailList)
         }
     },
     created(){
