18f5741b22439cd07699b8d385bd78cede781dda
PalumboAndrea <andreaseiuno.ap@gmail.com>
2022-12-21 15:38:49 +0100

    add generation of the email list and its display on the page

--- js/myscript.js
+++ js/myscript.js
@@ -3,10 +3,25 @@ const { createApp } = Vue;
 createApp({
     data()  {
         return {
-            
+            emailList: [],
         }
     },
     methods: {
-
+        getEmail(){
+            for(i=0; i<10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then( (response) => {
+                console.log(response.data.response)
+                this.emailList.push(response.data.response);
+                
+            })
+            
+            }
+            console.log(this.emailList)
+        }
+    },
+    created(){
+        this.getEmail()
     }
 }).mount('#app')
+
